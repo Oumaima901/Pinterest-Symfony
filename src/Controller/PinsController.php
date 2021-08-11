45a4b0976ab52cf4864393a3f5f7c3aeca167159
Oumaima <oumaimabk168@gmail.com>
2021-08-11 19:01:59 +0200

    Refactoring of create/edit by using PinType and make use of PUT as method for edit

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,13 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class PinsController extends AbstractController
@@ -18,53 +17,45 @@ class PinsController extends AbstractController
      * @Route("/", name="app_home" , methods="GET")
      */
     public function index(PinRepository $pinRepository): Response
-    {   
+    {
         $pins = $pinRepository->findBy([], ['createdAt' => 'ASC']);
         return $this->render('pins/index.html.twig', compact('pins'));
     }
-      /**
+    /**
      * @Route("/pins/create", name="app_pins_create" , methods="GET|POST")
      */
     public function create(Request $request, EntityManagerInterface $em): Response
-    {   
+    {
         $pin = new Pin;
-        $form = $this->createFormBuilder($pin)
-        ->add('title', TextType::class)
-        ->add('description', TextareaType::class)
-        ->getForm()
-        ;
+        $form = $this->createForm(PinType::class, $pin); 
+            
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-           $em->persist($pin);
-           $em->flush(); 
-           return $this->redirectToRoute('app_home');
-        } 
-        return $this->render('pins/create.html.twig',['monformulaire' => $form->createView()]);  
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($pin);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pins/create.html.twig', ['monformulaire' => $form->createView()]);
     }
-            /**
+    /**
      * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit" , methods="GET|POST")
      */
-    public function edit(Pin $pin,Request $request, EntityManagerInterface $em): Response
-    {  
-        $form = $this->createFormBuilder($pin)
-        ->add('title', TextType::class)
-        ->add('description', TextareaType::class)
-        ->getForm()
-        ;
+    public function edit(Pin $pin, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PinType::class, $pin); 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-          
-           $em->flush(); 
-           return $this->redirectToRoute('app_home');
-        } 
-        return $this->render('pins/edit.html.twig',['pin' =>$pin,'monformulaire' => $form->createView()]);  
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pins/edit.html.twig', ['pin' => $pin, 'monformulaire' => $form->createView()]);
     }
-        /**
+    /**
      * @Route("/pins/{id<[0-9]+>}", name="app_pins_show" , methods="GET")
      */
     public function show(Pin $pin): Response
-    {   
-        return $this->render('pins/show.html.twig', compact('pin'));  
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
-    
 }
